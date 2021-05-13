fabf94299e0bce64d91dd7fa11a2ba74982f015f
sanjana <sanjana@192.168.0.7>
2021-05-13 11:18:57 +0530

    Strings

---
+++ strings/src/strings/Five.java
@@ -0,0 +1,20 @@
+package strings;
+
+
+import java.util.*;
+public class Five {
+	public static String
+    removeFirstandLast(String str)
+    {
+
+        str = str.substring(1, str.length() - 1);
+        return str;
+    }
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.print(
+            removeFirstandLast(str));
+    }
+}
---
+++ strings/src/strings/Four.java
@@ -0,0 +1,10 @@
+package strings;
+import java.util.*;
+public class Four {
+	public static void main(String[] args)
+	 {
+		Scanner sc=new Scanner(System.in);
+	    String str = sc.nextLine();    
+	    System.out.println(str.substring(0, str.length()/2));	
+	  } 
+}
